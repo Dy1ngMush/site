95315deb7afb8a46bcb07dbb290128149e87da86
Gikann <K.Timyr.A@yandex.ru>
2024-06-02 17:05:21 +0300

    Create database config

--- MyShop/core/config.py
+++ MyShop/core/config.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
-
+from pydantic import PostgresDsn
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
+
+
+class DataBaseConfig(BaseModel):
+    url = PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
 
 
 class ApiPrefix(BaseModel):
@@ -14,6 +22,7 @@ class ApiPrefix(BaseModel):
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
 
 
 settings = Settings()
