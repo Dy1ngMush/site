8e8fda9f99370c14f8a674137279bfa666e8aa3c
Gikann <K.Timyr.A@yandex.ru>
2024-06-02 17:44:43 +0300

    Config read from env

--- MyShop/core/config.py
+++ MyShop/core/config.py
@@ -1,25 +1,32 @@
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import PostgresDsn
+
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
 
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
 class DataBaseConfig(BaseModel):
-    url = PostgresDsn
+    url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     max_overflow: int = 10
     pool_size: int = 50
 
 
-class ApiPrefix(BaseModel):
-    prefix: str = "/api"
-
-
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI__"
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DataBaseConfig
