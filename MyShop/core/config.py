6b90248e028c97f49e509f3e520b1df91ad3b0e6
Gikann <K.Timyr.A@yandex.ru>
2024-06-03 23:10:19 +0300

    optimization

--- MyShop/core/config.py
+++ MyShop/core/config.py
@@ -11,6 +11,7 @@ class RunConfig(BaseModel):
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
     users: str = "/users"
+    products: str = "/products"
 
 
 class ApiPrefix(BaseModel):
@@ -26,19 +27,20 @@ class DataBaseConfig(BaseModel):
     pool_size: int = 50
 
     naming_convention: dict[str, str] = {
-      "ix": "ix_%(column_0_label)s",
-      "uq": "uq_%(table_name)s_%(column_0_N_name)s",
-      "ck": "ck_%(table_name)s_%(constraint_name)s",
-      "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-      "pk": "pk_%(table_name)s"
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
     }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
-        env_prefix="FASTAPI__"
+        env_prefix="FASTAPI__",
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
