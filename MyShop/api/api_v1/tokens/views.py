fd84e6b89be85bef50840176dad2cb2f7b6247f0
Gikann <K.Timyr.A@yandex.ru>
2024-06-10 22:56:36 +0300

    added views for token

---
+++ MyShop/api/api_v1/tokens/views.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
+from core.models import db_helper
+from . import crud
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .dependencies import token_by_id
+from .schemas import Token
+from ..users.schemas import UserCreate
+
+router = APIRouter(tags=["Tokens"])
+
+
+@router.post("", response_model=Token, status_code=status.HTTP_201_CREATED)
+async def create_token(
+    user_data: UserCreate,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    return await crud.create_token(session=session, user_data=user_data)
+
+
+@router.delete("/{token_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_token(
+    token: Token = Depends(token_by_id),
+    session: AsyncSession = Depends(db_helper.session_getter),
+) -> None:
+    await crud.delete_token(
+        session=session,
+        token=token,
+    )
