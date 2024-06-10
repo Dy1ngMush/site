ba1041e379d8483d5f921189d1b7ab98f361a016
Gikann <K.Timyr.A@yandex.ru>
2024-06-10 22:56:16 +0300

    add token to init model

--- MyShop/core/models/__init__.py
+++ MyShop/core/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ("db_helper", "Base", "User", "Product", "Profile")
+__all__ = ("db_helper", "Base", "User", "Product", "Profile", "Token")
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
 from .product import Product
 from .profile import Profile
+from .token import Token
