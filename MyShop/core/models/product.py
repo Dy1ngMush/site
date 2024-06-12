c2b88a3d82982b48f3f92c22d75ed0257023b333
Gikann <K.Timyr.A@yandex.ru>
2024-06-12 04:11:59 +0300

    add product relationships with OrderProductAssociation

--- MyShop/core/models/product.py
+++ MyShop/core/models/product.py
@@ -1,6 +1,11 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .order_product_association import OrderProductAssociation
 
 
 class Product(Base):
@@ -8,3 +13,8 @@ class Product(Base):
     description: Mapped[str]
     price: Mapped[int]
     quantity: Mapped[int]
+
+
+    orders_details: Mapped[list["OrderProductAssociation"]] = relationship(
+        back_populates="product",
+    )
