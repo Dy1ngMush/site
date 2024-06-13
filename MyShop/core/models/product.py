521d0aa478114e84194e213b9f88ca90d7075906
Gikann <K.Timyr.A@yandex.ru>
2024-06-13 09:20:20 +0300

    added relations with true_order

--- MyShop/core/models/product.py
+++ MyShop/core/models/product.py
@@ -6,6 +6,7 @@ from .base import Base
 
 if TYPE_CHECKING:
     from .order_product_association import OrderProductAssociation
+    from .trueorder_product_association import TrueOrderProductAssociation
 
 
 class Product(Base):
@@ -14,7 +15,10 @@ class Product(Base):
     price: Mapped[int]
     quantity: Mapped[int]
 
-
     orders_details: Mapped[list["OrderProductAssociation"]] = relationship(
+        back_populates="product",
+    )
+
+    true_orders_details: Mapped[list["TrueOrderProductAssociation"]] = relationship(
         back_populates="product",
     )
