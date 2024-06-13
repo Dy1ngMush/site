8f8c9778eda2ec3860a242f634e0d0e9fc5c6405
Gikann <K.Timyr.A@yandex.ru>
2024-06-13 09:20:51 +0300

    add order and truorder relationship to user

--- MyShop/core/models/user.py
+++ MyShop/core/models/user.py
@@ -8,7 +8,8 @@ from .base import Base
 if TYPE_CHECKING:
     from .token import Token
     from .profile import Profile
-
+    from .order import Order
+    from .trueorder import TrueOrder
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=False)
@@ -16,8 +17,10 @@ class User(Base):
     password: Mapped[str] = mapped_column(String(128))
     active: Mapped[bool] = mapped_column(Boolean, default=True)
 
+    order: Mapped["Order"] = relationship(back_populates="user")
     profile: Mapped["Profile"] = relationship(back_populates="user")
     token: Mapped["Token"] = relationship(back_populates="user")
+    trueorder: Mapped["TrueOrder"] = relationship(back_populates="user")
 
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
