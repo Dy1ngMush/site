554ef3607acf6856e6a703dfc94140a94884bc44
Gikann <K.Timyr.A@yandex.ru>
2024-06-13 09:19:59 +0300

    fixed model order

--- MyShop/core/models/order.py
+++ MyShop/core/models/order.py
@@ -4,19 +4,15 @@ from uuid import UUID
 from sqlalchemy.orm import Mapped, relationship
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
-    from .product import Product
     from .order_product_association import OrderProductAssociation
 
 
-class Order(Base):
-    promocode: Mapped[str | None]
-    user_id: Mapped[UUID]
-    # products: Mapped[list["Product"]] = relationship(
-    #     secondary="order_product_association",
-    #     back_populates="orders",
-    # )
+class Order(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "order"
 
     products_details: Mapped[list["OrderProductAssociation"]] = relationship(
         back_populates="order"
