887ac6011f4af0cf00f2e205a9e522d0fbb1bc0e
Gikann <K.Timyr.A@yandex.ru>
2024-07-24 19:38:49 +0300

    add mounts on img, css and js

--- MyShop/main.py
+++ MyShop/main.py
@@ -1,12 +1,20 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi.responses import ORJSONResponse
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import ORJSONResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
 from core.config import settings
 from api import router as api_router
 from core.models import db_helper
 
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost",
+    "http://127.0.0.1"
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,8 +28,10 @@ main_app = FastAPI(
     lifespan=lifespan,
 )
 templates = Jinja2Templates(directory='pages')
+main_app.mount('/static/', StaticFiles(directory='pages/static'), name='pages/static')
+main_app.mount('/js/', StaticFiles(directory='pages/js'), name='pages/js')
+main_app.mount('/CSS/', StaticFiles(directory='pages/css'), name='pages/css')
 main_app.include_router(api_router)
-
 if __name__ == "__main__":
     uvicorn.run(
         app="main:main_app",
